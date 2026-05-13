c6645fa1c1cd675ceb9f589aa69efef2b8a22b5f
Gus <gus.flores89@gmail.com>
2026-05-13 00:40:19 -0300

    Fix math curriculum pedagogical order

--- ingreso-monse-2027/src/lib/curriculum.js
+++ ingreso-monse-2027/src/lib/curriculum.js
@@ -1,22 +1,27 @@
 export const CURRICULUM_MATEMATICA = [
-  { semanas: "1-2", tema: "graficos_estadisticos", orden: 1, materia: "matematica" },
-  { semanas: "2-3", tema: "fracciones_concepto", orden: 2, materia: "matematica" },
-  { semanas: "3-4", tema: "numeros_naturales_sistema_decimal", orden: 3, materia: "matematica" },
-  { semanas: "4-5", tema: "numeros_romanos", orden: 4, materia: "matematica" },
-  { semanas: "5-6", tema: "divisibilidad_primos_mcm_mcd", orden: 5, materia: "matematica" },
-  { semanas: "6-7", tema: "fracciones_operaciones", orden: 6, materia: "matematica" },
-  { semanas: "7-8", tema: "fracciones_del_resto", orden: 7, materia: "matematica" },
-  { semanas: "8-9", tema: "secuencias_aritmeticas", orden: 8, materia: "matematica" },
-  { semanas: "9-10", tema: "secuencias_geometricas_fibonacci", orden: 9, materia: "matematica" },
-  { semanas: "10-11", tema: "si_me_la_longitud", orden: 10, materia: "matematica" },
-  { semanas: "11-12", tema: "si_me_la_masa_capacidad_tiempo", orden: 11, materia: "matematica" },
-  { semanas: "12-14", tema: "geometria_angulos", orden: 12, materia: "matematica" },
-  { semanas: "14-16", tema: "geometria_triangulos", orden: 13, materia: "matematica" },
-  { semanas: "16-18", tema: "geometria_cuadrilateros_circunferencia", orden: 14, materia: "matematica" },
-  { semanas: "18-20", tema: "perimetros_simples", orden: 15, materia: "matematica" },
-  { semanas: "20-22", tema: "perimetros_compuestos", orden: 16, materia: "matematica" },
-  { semanas: "22-24", tema: "proporcionalidad_regla_3", orden: 17, materia: "matematica" },
-  { semanas: "24-26", tema: "ecuaciones_con_imagenes_2_incognitas", orden: 18, materia: "matematica" },
+  { semanas: "1-2", tema: "tablas_multiplicar_2_5", orden: 1, materia: "matematica" },
+  { semanas: "3-4", tema: "tablas_multiplicar_6_8", orden: 2, materia: "matematica" },
+  { semanas: "5-6", tema: "tablas_multiplicar_9_10", orden: 3, materia: "matematica" },
+  { semanas: "7-8", tema: "division_1_digito", orden: 4, materia: "matematica" },
+  { semanas: "9-10", tema: "fracciones_concepto", orden: 5, materia: "matematica" },
+  { semanas: "11-12", tema: "fracciones_operaciones", orden: 6, materia: "matematica" },
+  { semanas: "13-14", tema: "fracciones_del_resto", orden: 7, materia: "matematica" },
+  { semanas: "15-16", tema: "decimales_conversion", orden: 8, materia: "matematica" },
+  { semanas: "17-18", tema: "numeros_naturales_sistema_decimal", orden: 9, materia: "matematica" },
+  { semanas: "18-19", tema: "numeros_romanos", orden: 10, materia: "matematica" },
+  { semanas: "19-20", tema: "divisibilidad_primos_mcm_mcd", orden: 11, materia: "matematica" },
+  { semanas: "21-23", tema: "geometria_angulos", orden: 12, materia: "matematica" },
+  { semanas: "23-25", tema: "geometria_triangulos", orden: 13, materia: "matematica" },
+  { semanas: "25-27", tema: "geometria_cuadrilateros_circunferencia", orden: 14, materia: "matematica" },
+  { semanas: "27-29", tema: "perimetros_simples", orden: 15, materia: "matematica" },
+  { semanas: "29-31", tema: "perimetros_compuestos", orden: 16, materia: "matematica" },
+  { semanas: "31-32", tema: "si_me_la_longitud", orden: 17, materia: "matematica" },
+  { semanas: "32-33", tema: "si_me_la_masa_capacidad_tiempo", orden: 18, materia: "matematica" },
+  { semanas: "33-34", tema: "graficos_estadisticos", orden: 19, materia: "matematica" },
+  { semanas: "34-35", tema: "proporcionalidad_regla_3", orden: 20, materia: "matematica" },
+  { semanas: "35-36", tema: "secuencias_aritmeticas", orden: 21, materia: "matematica" },
+  { semanas: "36-37", tema: "secuencias_geometricas_fibonacci", orden: 22, materia: "matematica" },
+  { semanas: "37-38", tema: "ecuaciones_con_imagenes_2_incognitas", orden: 23, materia: "matematica" },
 ];
 
 export const CURRICULUM_LENGUA = [
@@ -36,7 +41,7 @@ export const CURRICULUM_LENGUA = [
   { semanas: "16-20", tema: "produccion_escrita_narracion", orden: 14, materia: "lengua" },
 ];
 
-export const DEFAULT_TOPIC = "graficos_estadisticos";
+export const DEFAULT_TOPIC = "tablas_multiplicar_2_5";
 
 export function getCurriculumByMateria(materia) {
   return materia === "lengua" ? CURRICULUM_LENGUA : CURRICULUM_MATEMATICA;
